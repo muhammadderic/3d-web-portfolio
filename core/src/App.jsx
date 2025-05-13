e16db82473052b949be6463d8cea6cd33fe5b16c
muhammadderic <muhammad.donny.e@gmail.com>
2025-05-13 11:11:11 +0700

    f: create Navbar section

--- core/src/App.jsx
+++ core/src/App.jsx
@@ -1,11 +1,13 @@
 import { 
   About, 
-  Hero 
+  Hero, 
+  Navbar
 } from "./sections"
 
 function App() {
   return (
     <main className="max-w-7xl mx-auto relative">
+      <Navbar />
       <Hero />
       <About />
     </main>
--- core/src/sections/index.js
+++ core/src/sections/index.js
@@ -1,2 +1,3 @@
+export { Navbar } from "./Navbar";
 export { Hero } from "./Hero";
 export { About } from "./About";
